0c96564a5aac07244c706243c9ec86d1bcc0d594
guibedan <guilhermebedan123@gmail.com>
2022-10-25 20:56:04 -0300

    javaEstudo

---
+++ src/dio/edu/intro/Main.java
@@ -0,0 +1,19 @@
+package dio.edu.intro;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int soma = Nup(2,3);
+
+        System.out.println(soma);
+
+    }
+
+    static int Nup(int n1, int n2) {
+        return n1 + n2;
+    }
+
+}
